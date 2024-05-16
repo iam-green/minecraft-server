794dabff4f1712564f7e10302d36d72446c9e461
Dohyeon Yun <yundohyun050121@gmail.com>
2024-05-16 22:23:40 +0900

    add: `server.bat` download powershell script

--- server.bat
+++ server.bat
@@ -1,3 +1,6 @@
 @echo off
+if not exist "server.ps1" (
+  powershell Invoke-WebRequest -Uri "https://raw.githubusercontent.com/Past2l/minecraft-server/main/server.ps1" -OutFile "server.ps1"
+)
 PowerShell.exe -ExecutionPolicy RemoteSigned -File .\server.ps1 %*
 pause
