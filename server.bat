b325f228917a4f7f7aef4419da427233f90fa721
Pastel <yundohyun050121@gmail.com>
2024-05-22 14:19:23 +0900

    remove: `server.bat` pause

--- server.bat
+++ server.bat
@@ -3,4 +3,3 @@ if not exist "server.ps1" (
   powershell curl.exe -sfSLo .\server.ps1 "https://raw.githubusercontent.com/Past2l/minecraft-server/main/server.ps1"
 )
 PowerShell.exe -ExecutionPolicy RemoteSigned -File .\server.ps1 %*
-pause
